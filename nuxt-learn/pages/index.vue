ba7304b720b39e3db253b7886036bf796e910baa
gtt011029 <17356136872@163.com>
2019-02-16 18:59:02 +0800

    初始化

---
+++ nuxt-learn/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+ <div>
+   首页（葛李俊）
+   <h1>我是index首页</h1>
+   <nuxt-link to="mynews">go news</nuxt-link>
+ </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  }
+}
+</script>
+
+<style>
+
+</style>
